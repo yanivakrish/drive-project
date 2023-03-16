719e67f8fbb59e71594b876808d6cddff6a19585
Yaniv Akrish <yanivakrish@gmail.com>
2023-03-16 10:03:48 +0200

    feat(store): initialize redux store

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { App } from './app';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
