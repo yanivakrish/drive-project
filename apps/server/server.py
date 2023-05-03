c011fe4c8228e14374b503bf6b7803f86d28b958
Yaniv Askrish <yanivakrish@gmail.com>
2023-05-03 13:45:59 +0300

    feat(server): finish work on server. All 3 routes have been implemented

--- apps/server/server.py
+++ apps/server/server.py
@@ -1,9 +1,12 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, send_file
 from pymongo import MongoClient
+from bson import ObjectId, json_util
 import gridfs
 
-db = MongoClient('mongodb://localhost:27017').drive_database
-fs = gridfs.GridFS(db)
+client = MongoClient('mongodb://localhost:27017')
+db = client['drive_database']
+fs = gridfs.GridFS(db, collection='fs')
+files_collection = db['fs.files']
 
 app = Flask(__name__)
 
@@ -13,13 +16,25 @@ GET, POST = 'GET', 'POST'
 @app.route('/upload-file', methods=[POST])
 def upload_file():
     uploaded_file = request.files['file']
-    id = fs.put(uploaded_file)
+    id = fs.put(uploaded_file,  filename=uploaded_file.filename)
     return {'message': str(id)}
 
 
 @app.route('/get-files', methods=[GET])
 def get_files():
-    return db['fs.files'].find()
+    file_docs = list(files_collection.find(
+        {}, projection={'_id': 1, 'filename': 1}))
+    file_ids = [{"id": str(file_doc['_id']), "filename": str(
+        file_doc['filename'])} for file_doc in file_docs]
+    return json_util.dumps({'file_ids': file_ids})
+
+
+@app.route('/download-file', methods=[POST])
+def download_file():
+    data = request.get_json()
+    file_id = ObjectId(data['id'])
+    file = fs.get(file_id=file_id)
+    return send_file(file, as_attachment=True, download_name=file.filename)
 
 
 if __name__ == '__main__':
